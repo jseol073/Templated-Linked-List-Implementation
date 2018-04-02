a975c680874fa3eca1bdbd6ca0b27b13a93da845
johnseol <kseol4@illinois.edu>
2018-04-02 11:06:04 -0500

    set up Xcode

--- src/ll.cpp
+++ src/ll.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 namespace snakelinkedlist {
     
-    LinkedList::LinkedList() {
-        
+    LinkedList::LinkedList() : list_head(NULL) {
     }
     
     LinkedList::LinkedList(const std::vector<SnakeBodySegment>& values) {
@@ -13,7 +12,36 @@ namespace snakelinkedlist {
     }
     
     LinkedList::LinkedList(const LinkedList& source) {
-        
+        if (source.list_head) {
+            list_head = new Node(*source.list_head);
+        } else {
+            list_head = nullptr;
+        }
     }
+    
+    LinkedList::LinkedList(LinkedList&& source) noexcept {
+        list_head = source.list_head;
+        source.list_head = nullptr;
+    }
+    
+    LinkedList::~LinkedList() {
+        delete list_head;
+    }
+    
+//    LinkedList::LinkedList& operator=(const LinkedList& source) {
+//        if(this == &source) {
+//            return;
+//        }
+//        delete list_head;
+//        list_head = nullptr;
+//        if(source.list_head) {
+//            list_head = new Node(*source.list_head);
+//        }
+//        return *this;
+//    }
+    
+//    bool operator!=(const LinkedList& lhs, const LinkedList& rhs) {
+//
+//    }
 
 } // namespace snakelinkedlist
